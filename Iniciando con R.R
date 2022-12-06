0d92dfeb2aba2ac5067cd432d8e1687f69e09bf7
David Betancur <juan.david.betancur@outlook.com>
2022-12-06 11:08:47 -0500

    ggplot geom_bars y plotbox
    Realizar ejercicios de geom_bars y plotbox

--- Iniciando con R.R
+++ Iniciando con R.R
@@ -47,4 +47,71 @@ ggplot(data = mpg, mapping = aes(x=displ, y=hwy)) +
 ggplot(data = mpg, mapping = aes(x=displ, y=hwy, color=drv)) + 
   geom_point()+
   geom_smooth(se=FALSE)
+
+ggplot(data = diamonds)+
+  geom_bar(mapping = aes(x=cut))
+
+?geom_bar
+
+demo <- tribble(
+  ~a, ~b, 
+  "bar1",20,
+  "bar2",30,
+  "bar3",40
+)
+ggplot(data = demo)+
+  geom_bar(mapping = aes(x=a, y=b), stat="identity")
   
+ggplot(data = diamonds)+
+  stat_summary(
+    mapping = aes(x=cut, y=depth),
+    fun.ymax = max,
+    fun.ymin = min,
+    fun.y = median
+  )
+
+ggplot(data = diamonds)+
+  geom_bar(mapping = aes(x=cut, color= cut))
+
+ggplot(data = diamonds)+
+  geom_bar(mapping = aes(x=cut, fill= cut))
+
+ggplot(data = diamonds)+
+  geom_bar(mapping = aes(x=cut, fill= clarity))
+
+ggplot(data = diamonds)+
+  geom_bar(mapping = aes(x=cut, fill= clarity), position = "fill")
+
+ggplot(data = diamonds)+
+  geom_bar(mapping = aes(x=cut, fill= clarity), position = "dodge")
+
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x=displ, y=hwy, color=manufacturer), position="jitter")
+
+ggplot(data = mpg)+
+  geom_jitter(mapping = aes(x=displ, y=hwy))
+
+ggplot(data = mpg, mapping = aes(x=class, y=hwy))+
+  geom_boxplot()
+
+ggplot(data = mpg, mapping = aes(x=class, y=hwy))+
+  geom_boxplot()+
+  coord_flip()
+
+bar<- ggplot(data=diamonds)+
+  geom_bar(
+    mapping = aes(x=cut, fill=cut),
+    show.legend = FALSE,
+    width = 1
+  )+
+  theme(aspect.ratio = 1)+
+  labs(x=NULL,y=NULL)
+
+bar+coord_flip()
+bar+coord_polar()
+
+ggplot(data = mpg, mapping = aes(x=cty, y=hwy))+
+  geom_point()+
+  geom_abline()+
+  coord_fixed()
+
