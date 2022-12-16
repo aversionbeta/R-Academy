91c4481855b337379301e14e6d1f79d463b79b6e
David Betancur <juan.david.betancur@outlook.com>
2022-12-15 19:11:45 -0500

    dyplr introduction
    Exercises about dyplr

---
+++ dyplr.R
@@ -0,0 +1,32 @@
+y <- seq(1,10, length.out=5)
+y
+
+install.packages("nycflights13")
+
+?flights
+
+(jan1 <- filter(flights, month == 1, day == 1))
+
+filter(flights, month %in% c(11,12))
+
+# Flights that had an arrival delay of two or more hours
+filter(flights, arr_delay >= 120)
+
+# All Flights to Houston (IAH or HOU)
+filter(flights, dest %in% c("IAH", "HOU"))
+
+# All flights that were operated by United, American, or Delta
+filter(flights, carrier %in% c("United", "American", "Delta"))
+
+# All flights that departed in Summer (July, August, September)
+filter (flights, month %in% c(7,8,9) )
+
+# All flights that arrived more than two hours late, but didn't leave late 
+filter(flights, arr_delay > 120, dep_delay == 0 )
+
+# All flights that were delayed by at least an hour, but made up over 30 minutes flight
+filter(flights, dep_delay>60, air_time>30)
+
+# All flights that departed between midnight and 6 am 
+filter(flights, between(dep_time, 2400, 6000))
+
